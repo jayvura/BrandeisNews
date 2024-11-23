931fa9e87fa15737342168107fa582ac420123dd
Jayden <jdecambre10@gmail.com>
2024-11-23 15:06:56 -0500

    Initial Commit

---
+++ BrandeisHoot/BrandeisHootApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrandeisHootApp.swift
+//  BrandeisHoot
+//
+//  Created by Jayden on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BrandeisHootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrandeisHootTests/BrandeisHootTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrandeisHootTests.swift
+//  BrandeisHootTests
+//
+//  Created by Jayden on 11/23/24.
+//
+
+import Testing
+@testable import BrandeisHoot
+
+struct BrandeisHootTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
